4886053be85f44cc533d266b42ccdf13a8b65431
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-02-20 23:33:57 +0100

    Fix jquery link, Finish app.js

--- 2-JS/app.js
+++ 2-JS/app.js
@@ -42,9 +42,84 @@ function Column(name) {
 	}
 }
 
+Column.prototype = {
+	addCard: function(card) {
+		this.$element.children('ul').append(card.$element);
+	},
+	removeColumn: function() {
+		this.$element.remove();
+	}
+};
 
+//.......................................................
 
+function Card(description) {
+	var self = this;
+	this.id = randomString();
+	this.description = description;
+	this.$element = createCard();
 
+	function createCard(name) {
+		var $card = $('<li>').addClass('card');
+		var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+		var $cardDelete = $('<button>').addClass('btn-delete').text('x');
 
+		$cardDelete.click(function() {
+			self.removeCard();
+		});
+
+		$card.append($cardDelete)
+			.append($cardDescription);
+		return $card;
+	}
+}
+
+Card.prototype = {
+	removeCard: function() {
+		this.$element.remove();
+	}
+};
+
+//.......................................................
+
+var board = {
+	name: 'Kanban Board',
+	addColumn: function(column) {
+		this.$element.append(column.$element);
+		initSortable();
+	},
+	$element: $('#board .column-container')
+};
+
+function initSortable() {
+	$('.column-card-list').sortable({
+		connectWith: '.column-card-list',
+		placeholder: 'card-placeholder'
+	}).disableSelection();
+}
+
+$('.create-column').click(function() {
+	var name = prompt('Enter a column name:');
+	var column = new Column(name);
+	board.addColumn(column);
+});
+
+//..............................................
+
+var todoColumn = new Column('To do');
+var doingColumn = new Column('Doing');
+var doneColumn = new Column('Done');
+
+board.addColumn(todoColumn);
+board.addColumn(doingColumn);
+board.addColumn(doneColumn);
+
+var card1 = new Card('New task');
+var card2 = new Card('Create kanban boards');
+
+todoColumn.addCard(card1);
+doingColumn.addCard(card2);
+
+//................................................
 //END
-})
+});
